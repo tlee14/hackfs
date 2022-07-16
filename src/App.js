f59dde1ee9f1736119127a56df881a166b5a9ae3
Grant Schneider <grant@upstart.com>
2022-07-15 22:29:31 -0400

    Add ability to upload to IPFS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 
 import { useEffect, useState } from 'react';
+import { create } from 'ipfs-http-client';
 import Web3 from 'web3';
+
+const client = create('https://ipfs.infura.io:5001/api/v0')
 
 function App() {
   const [account, setAccount] = useState(); // state variable to set account.
+  const [fileUrl, updateFileUrl] = useState(``)
+
+  async function onChange(e) {
+    const file = e.target.files[0]
+    try {
+      const added = await client.add(file)
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      updateFileUrl(url)
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }
+  }
 
   useEffect(() => {
     async function load() {
@@ -15,13 +30,19 @@ function App() {
     }
 
     load();
-   }, []);
+  }, []);
 
-   return (
-     <div>
-       Welcome! Your account is: {account}
-     </div>
-   );
+  return (
+    <div>
+      <p> Welcome! Your account is: <b>{account}</b></p>
+      <p> Upload a file to IPFS: </p>
+      <input type="file" onChange={onChange} />
+        {
+          "Your IPFS file is available at: " + fileUrl
+        }
+
+    </div>
+  );
 }
 
 export default App;
